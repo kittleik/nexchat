be6485e6379da554b5774304945a0613c5b04dd5
Ginger Hero <grimholt@gmail.com>
2024-08-23 00:09:29 +0200

    La Home.vue og NewPost.vue inn i en views folder istedenfor components

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../components/Home.vue';
-import NewPost from '../components/NewPost.vue';
+import Home from '../views/Home.vue';
+import NewPost from '../views/NewPost.vue';
 
 const routes = [
     {
